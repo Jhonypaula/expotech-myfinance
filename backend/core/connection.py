e1817cf1ca816cde674c3de4d15f9115515cd39f
jhonypaula <jhony.depaula06@gmail.com>
2026-06-12 16:54:36 -0300

    fix: Refatora a função conectar_banco para lidar com erros de conexão.

--- backend/core/connection.py
+++ backend/core/connection.py
@@ -11,13 +11,16 @@ load_dotenv(dotenv_path=env_path)
 
 load_dotenv() 
 
-def conectar_banco  ():
-    
-    conexao = mysql.connector.connect (
-        host = os.getenv("DB_HOST"),
-        user = os.getenv("DB_USER"),
-        password = os.getenv("DB_PASSWORD"),
-        database = os.getenv("DB_NAME")   
-    )
+def conectar_banco():
+    try:
+        return mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
 
-    return conexao
+    except mysql.connector.Error:
+        raise ConnectionError(
+            "Não foi possível conectar ao banco de dados."
+        )
